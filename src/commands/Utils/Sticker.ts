387057f5364044cb6cacb869ad857d5cd40e0924
Cui Kun 真 <92380425+Cuizumi@users.noreply.github.com>
2022-01-28 16:09:49 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "🌟 Here you go ",
-						author: pack[2] || "Chitoge 🌟",
+						pack: pack[1] || "Shuna🌸",
+						author: pack[2] || "Synbee sama❄️",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
